e275a2e8302847ec5b8e5eceb2608ae7ad789f71
RSand18 <43037672+RSand18@users.noreply.github.com>
2018-11-28 10:39:56 -0600

    Added Feedback for checkpoints, Color and Scale changes

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,7 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour {
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 300;
+
+    [SerializeField]
+    private float inactivatedScale = 1, activatedScale = 1.5f;
+
+    [SerializeField]
+    private Color inactivatedcolor, activatedColor;
+
+    private bool isActivated = false;
+    private SpriteRenderer spriterenderer;
+
+    private void Start()
+    {
+        spriterenderer = GetComponent<SpriteRenderer>();
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    private void UpdateColor()
+    {
+        Color color = inactivatedcolor;
+
+        if (isActivated)
+        {
+            color = activatedColor;
+        }
+        spriterenderer.color = color;
+    }
+    private void UpdateScale()
+    {
+        float scale = inactivatedScale;
+
+        if (isActivated)
+        {
+            scale = activatedScale;
+        }
+
+        transform.localScale = Vector3.one * scale;
+    }
+
+    private void UpdateRotation()
+    {
+        float rotationSpeed = inactivatedRotationSpeed;
+
+        if (isActivated)
+        {
+            rotationSpeed = activatedRotationSpeed;
+        }
+
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
+        UpdateScale();
+        UpdateColor();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
